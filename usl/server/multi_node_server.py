294f09a2bda5f7a61999446465f676f622a1ab78
ZJUWalker <1723044191@qq.com>
2025-11-18 11:50:26 +0800

    update multi node server

--- usl/server/multi_node_server.py
+++ usl/server/multi_node_server.py
@@ -1,6 +1,7 @@
 from concurrent.futures import Future, ThreadPoolExecutor
 from contextlib import nullcontext
 from dataclasses import dataclass
+from abc import abstractmethod, ABC
 from queue import Queue, Empty
 import threading
 import logging
@@ -13,41 +14,16 @@ import torch.nn as nn
 import torch.distributed as dist
 import torch.multiprocessing as mp
 
+from usl.server.base import *
 from usl.server.single_server import PipelineMode, ServerArgs, SingleServer
 from usl.socket import SocketCommunicator, Payload
 from usl.utils.usl_gantt_plot import GanttChartData
 from usl.offload import CpuOffloadHookWithOffloadHandler, AsyncDoubleBufferGroupOffloadHandler
 
-
-# Copyright (c) Microsoft Corporation.
-# Licensed under the MIT license.
-
 import threading
 
-"""
-Implementation of a thread-safe counter with many producers and many consumers.
-"""
 
-
-class Counter:
-    def __init__(self, initial_count):
-        self.count = initial_count
-        self.cv = threading.Condition()
-
-    def decrement(self):
-        self.cv.acquire()
-        self.count -= 1
-        self.cv.notify_all()
-        self.cv.release()
-
-    def wait(self):
-        self.cv.acquire()
-        while self.count > 0:
-            self.cv.wait()
-        self.cv.release()
-
-
-class PipelineServer:
+class PipelineServer(ABC):
 
     def __init__(
         self,
@@ -59,13 +35,12 @@ class PipelineServer:
 
         # ---- Config & logging
         self.server_args = server_args
-        self.server_device = torch.cuda.device(dist.get_rank())
+        self.device = torch.cuda.device(dist.get_rank())
         self.logger: logging.Logger = logger or logging.getLogger(__name__)
 
         # ---- Profile Metrics
         self.max_cuda_memory_allocated = 0.0
         self.profile_data: List[GanttChartData] = []
-        self.global_step = 0
         self.server_fwd_time = 0
         self.server_fwd_send_time = 0
         self.server_bwd_time = 0
@@ -80,15 +55,15 @@ class PipelineServer:
         self.stop_event = threading.Event()
 
         # ---- CUDA streams
-        torch.cuda.set_stream(torch.cuda.Stream(self.server_device))  # set cuda compute stream
-        self.load_stream = torch.cuda.Stream(self.server_device)  # set cuda load stream
-        self.offload_stream = torch.cuda.Stream(self.server_device)  # set cuda offload stream
+        torch.cuda.set_stream(torch.cuda.Stream(self.device))  # set cuda compute stream
+        self.load_stream = torch.cuda.Stream(self.device)  # set cuda load stream
+        self.offload_stream = torch.cuda.Stream(self.device)  # set cuda offload stream
         self.start_event = torch.cuda.Event(enable_timing=True)
         self.end_event = torch.cuda.Event(enable_timing=True)
 
         # ---- Model & optimizer
         self.lr = server_args.learning_rate
-        self.trunk_model = server_model.to(self.server_device).train()
+        self.trunk_model = server_model.to(self.device).train()
         self.optimizer: torch.optim.Optimizer = optimizer_clz(self.trunk_model.parameters(), lr=self.lr)
         self.offload_activation_mb_num = self.server_args.offload_activation_mb_num
         if self.offload_activation_mb_num > 0:
@@ -103,6 +78,8 @@ class PipelineServer:
         self._init_pipeline_info()
 
     def _init_comminication(self):
+        # thread-safe queues for tensor send and recv
+        self._init_tensor_queues()
         # ---- Communicator
         # the first and last node need a socket to communicate with the client
         if self.rank == 0 or self.rank == self.world_size - 1:
@@ -117,26 +94,37 @@ class PipelineServer:
         self.world_size = dist.get_world_size()
         dist.init_process_group(backend="nccl")
 
-        # ---- Queues (compute pipeline)
-        self.activation_from_pre_rank_queue: Queue = Queue()
-        self.activation_to_next_rank_queue: Queue = Queue()
-        self.gradient_from_next_rank_queue: Queue = Queue()
-        self.gradient_to_pre_rank_queue: Queue = Queue()
-
         # ---- Micro-batch context/state
         # self.ctx: Dict[str, Dict[str, torch.Tensor]] = {}
         # self.group_state: Dict[str, Dict[str, Any]] = {}
 
+    def _init_tensor_queues(self):
+        # ---- Queues (compute pipeline)
+        # FWD activation queues
+        self.activation_from_pre_rank_queue: Queue[Tuple[int, int, torch.Tensor]] = Queue(maxsize=self.num_micro_batches)
+        self.activation_to_next_rank_queue: Queue[Tuple[int, int, torch.Tensor]] = Queue(maxsize=self.num_micro_batches)
+        # BWD gradient queues
+        self.gradient_from_next_rank_queue: Queue[Tuple[int, int, torch.Tensor]] = Queue(maxsize=self.num_micro_batches)
+        self.gradient_to_pre_rank_queue: Queue[Tuple[int, int, torch.Tensor]] = Queue(maxsize=self.num_micro_batches)
+        # ----------- FWD appendix queues -----------
+        # Attention mask queue
+        self.attention_mask_queue: Queue[Tuple[int, int, torch.Tensor]] = Queue(maxsize=self.num_micro_batches)
+        # Position embeddings queue (Model with ROPE)
+        # self.position_embeddings_queue: Queue[Tuple[int, int, Tuple[torch.Tensor, torch.Tensor]]] = Queue()
+
     def _init_pipeline_info(self):
         # ---- Pipeline info
+        self.global_batch_idx = 0
         self.pipeline_mode = self.server_args.pipeline_mode
         self.batch_size = self.server_args.batch_size
         self.micro_batch_size = self.server_args.micro_batch_size
         self.num_micro_batches = (self.batch_size + self.micro_batch_size - 1) // self.micro_batch_size
-        self.curr_mb_idx = 0
+        # TODO 不同流水线可能得不同地使用
+        self.fwd_mb_count = 0
+        self.bwd_mb_count = 0
 
     def _is_last_mb_of_global_batch(self) -> bool:
-        return self.curr_mb_idx == self.num_micro_batches - 1
+        return self.fwd_mb_count == self.num_micro_batches - 1
 
     def _init_param_efficient_mode(self):
         pass
@@ -160,23 +148,22 @@ class PipelineServer:
 
     def _compute_loop(self):
         try:
-            if self.server_args.pipeline_mode in [PipelineMode.GPIPE, PipelineMode.PIPE_DREAM_WC]:
-                self._compute_task_gpipe_or_pipe_dream_wc()
-            elif self.server_args.pipeline_mode == PipelineMode.PIPE_DREAM_STRICT:
-                self._compute_task_pipedream_strict()
-            elif self.server_args.pipeline_mode == PipelineMode.NAIVE:
-                self._compute_task_sequential()
-            else:
-                raise ValueError(f"Unknown pipeline mode: {self.server_args.pipeline_mode}")
+            self._compute_task()
         except Exception as e:
             self.logger.exception(f"Compute loop crashed: {e}")
             self.shutdown()
 
+    @abstractmethod
     def _recv_loop(self):
-        pass
+        raise NotImplementedError('Method not implemented')
 
+    @abstractmethod
     def _send_loop(self):
-        pass
+        raise NotImplementedError('Method not implemented')
+
+    @abstractmethod
+    def _compute_task(self):
+        raise NotImplementedError('Method not implemented')
 
     def shutdown(self):
         """Graceful shutdown for futures, sockets, and executor."""
@@ -199,48 +186,165 @@ class PipelineServer:
 
     @property
     def recv_fwd_acti_rank(self) -> int:
-        return self.rank - 1
+        return -1 if self.rank <= 0 else self.rank - 1
 
     @property
     def recv_bwd_grad_rank(self) -> int:
-        return self.rank + 1
+        return -1 if self.rank >= self.world_size - 1 else self.rank + 1
 
     @property
     def send_fwd_acti_rank(self) -> int:
-        return self.rank + 1
+        return self.recv_bwd_grad_rank  # same as recv_bwd_grad_rank
 
     @property
     def send_bwd_grad_rank(self) -> int:
-        return self.rank - 1  # same as recv_fwd_acti_rank
+        return self.recv_fwd_acti_rank  # same as recv_fwd_acti_rank
 
-    def _send_meta(self, payload: Optional[Payload | torch.Tensor], rank: int):
-
+    def _send_tensor(
+        self,
+        dst_rank: int,
+        retrive_queue: Queue[Tuple[int, int, torch.Tensor]],
+        block: bool = False,
+    ):
+        """
+        从对应队列里取出一个 (msg_type, tensor) 并发送, 由发送线程做
+        retrive_queue 里的元素形如: (msg_type, tensor)
+        Header 格式:
+        [
+            msg_type,         # 消息类型(1/2/3/N)
+            microbatch_idx,   # 当前流水线中 microbatch ID
+            ndim,             # 真实维度（几维）
+            dim0, dim1, ...   # 各维度大小
+            0, 0, ...         # padding
+        ]
+        """
+        if dst_rank == -1:
+            self._send_socket_msg(retrive_queue, block=block)
+        else:
+            self._send_nccl_msg(dst_rank, retrive_queue, block=block)
         pass
 
-    def _recv_meta(self, rank: int) -> Optional[Payload | torch.Tensor]:
+    def _put_tensor_to_queue(self, msg_type, microbatch_idx, tensor, block=False):
+        if msg_type == MSG_TYPE_ACTIVATION:
+            self.activation_to_next_rank_queue.put((msg_type, microbatch_idx, tensor), block=block)
+        elif msg_type == MSG_TYPE_GRADIENT:
+            self.gradient_to_pre_rank_queue.put((msg_type, microbatch_idx, tensor), block=block)
+        elif msg_type == MSG_TYPE_ATTENTION_MASK:
+            self.attention_mask_queue.put((msg_type, microbatch_idx, tensor), block=block)
+        else:
+            raise ValueError(f"Unknown msg_type: {msg_type}")
 
+    def _recv_tensor(self, src_rank: int, dtype: torch.dtype, block: bool = False):
+
+        if src_rank == -1:  # 从 socket 接收
+            payload = self._recv_socket_msg(src_rank, dtype, block)
+            if isinstance(payload, dict) and "stop" in payload.keys():
+                # print("Client requested stop") TODO
+                pass
+            else:
+                payload: Payload
+                msg_type = MSG_TYPE_ACTIVATION if payload.is_activation else MSG_TYPE_GRADIENT
+                mb_idx = payload.mb_idx
+                tensor = payload.tensor.to(self.device, dtype=dtype)
+                # 放到对应队列中
+                self._put_tensor_to_queue(msg_type, mb_idx, tensor, block)
+                if msg_type == MSG_TYPE_ACTIVATION:
+                    attention_mask_msg = (MSG_TYPE_ATTENTION_MASK, mb_idx, payload.attention_mask)
+                    self._put_tensor_to_queue(*attention_mask_msg, block=block)
+        else:
+            msg = self._recv_nccl_msg(src_rank, dtype, block)
+            # 放到对应队列中
+            self._put_tensor_to_queue(*msg, block=block)
+
+    @torch.no_grad()
+    def _recv_socket_msg(self) -> Union[Payload, Dict]:
+        """
+        接收 socket 消息
+        """
+        self.communicator.conn.settimeout(60.0)
+        data: Union[Payload, Dict] = self.communicator.receive()
+        return data
+
+    @torch.no_grad()
+    def _recv_nccl_msg(self, src_rank: int, dtype: torch.dtype) -> torch.Tensor:
+        """
+        接收一个张量并放到指定队列,由接收线程做
+        接收到的会以 (msg_type, tensor) 的形式放入 placement_queue
+        """
+        header_len = MAX_DIM + 3
+
+        # ----- 1) 接收 header -----
+        header_tensor: torch.Tensor = torch.empty(header_len, dtype=torch.int64, device=self.device)
+        dist.recv(header_tensor, src=src_rank, tag=0)
+
+        header_list = header_tensor.tolist()
+        msg_type = int(header_list[0])
+        microbatch_idx = int(header_list[1])
+        ndim = int(header_list[2])
+        dims = header_list[3 : 3 + ndim]
+        real_shape = [int(d) for d in dims]
+
+        # ----- 2) 接收 flat tensor -----
+        numel = int(torch.prod(torch.tensor(real_shape)))
+        flat_tensor: torch.Tensor = torch.empty(numel, dtype=dtype, device=self.device)
+        dist.recv(flat_tensor, src=src_rank, tag=1)
+
+        tensor = flat_tensor.view(*real_shape)
+        return (msg_type, microbatch_idx, tensor)
+
+    @torch.no_grad()
+    def _send_socket_msg(self, retrive_queue: Queue[Tuple[int, int, torch.Tensor]], block: bool = False):
+        """
+        发送 socket 消息
+        """
+        if retrive_queue == self.attention_mask_queue:
+            # 不用发送attention mask到 client 端
+            return
+        msg_type, microbatch_idx, tensor = retrive_queue.get(block=block)
+        if tensor.is_cuda:
+            cpu_tensor = tensor.cpu().pin_memory()
+        # send CPU payload to client
+        payload = Payload(
+            tensor=cpu_tensor,
+            is_activation=msg_type == MSG_TYPE_ACTIVATION,
+            phase='FWD' if msg_type == MSG_TYPE_ACTIVATION else 'BWD',
+            mb_idx=microbatch_idx,
+            mb_total=self.num_micro_batches,
+            attention_mask=None,
+            position_embeddings=None,
+            # TODO solve token and group_id in Payload
+        )
+        self.communicator.send(payload)
         pass
 
-    def _send_tensor(self, tensor: torch.Tensor, dst: int):
-        # --- 1. 发送 shape ---
-        shape_tensor = torch.tensor(list(tensor.shape), dtype=torch.int32, device=tensor.device)
+    @torch.no_grad()
+    def _send_nccl_msg(self, dst_rank: int, retrive_queue: Queue[Tuple[int, int, torch.Tensor]], block: bool = False):
+        """
+        发送 NCCL 消息
+        """
+        msg_type, microbatch_idx, tensor = retrive_queue.get(block=block)
 
-        # 设定全局的张量dim为3 [batch_size, seq_len, hidden_size],包括attention_mask,position_embeddings
-        # 告诉对方每一维的大小
-        dist.send(shape_tensor, dst)
+        assert tensor.is_cuda, "NCCL 要求 tensor 在 GPU 上"
+        shape = list(tensor.shape)
+        ndim = len(shape)
+        assert ndim <= MAX_DIM, f"tensor 维度 {ndim} > MAX_DIM={MAX_DIM}"
 
-        # --- 2. 发送数据 ---
+        # --- 1. 发送 header: [msg_type, ndim, dim0, dim1, ..., 0, 0] ---
+        header_len = MAX_DIM + 3
+        header = [msg_type, microbatch_idx, ndim] + shape
+        header += [0] * (header_len - len(header))
+
+        header_tensor = torch.tensor(header, dtype=torch.int64, device=tensor.device)
+        wk1 = dist.isend(header_tensor, dst=dst_rank, tag=0)  # 异步发送header
+
+        # --- 2. 发送数据本体（flatten） ---
         flat_tensor = tensor.contiguous().view(-1)
-        dist.send(flat_tensor, dst)
-
-    def _compute_task_gpipe_or_pipe_dream_wc(self):
-
+        wk2 = dist.isend(flat_tensor, dst=dst_rank, tag=1)
+        wk1.wait()
+        wk2.wait()
         pass
 
-    def _compute_task_pipedream_strict(self):
-
-        pass
-
-    def _compute_task_sequential(self):
-
-        pass
+    def _update_stage(self):
+        # update model
+        self.optimizer.step()
+        self.optimizer.zero_grad(set_to_none=True)
